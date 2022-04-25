9b14a33f0aee415e8f893e69da929b3c202ab461
Sergey Osokin <sergey.osokin@netfire.eu>
2022-04-25 03:59:42 +0200

    Updated some packages; smaller changes in configs.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,14 @@
 import { defineNuxtConfig } from 'nuxt3';
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     css: [
         '@/node_modules/bootstrap/scss/bootstrap.scss',
         '@/assets/styles/main.scss'
-    ]
-    // link: [
+    ],
+    typescript: {
+        shim: false
+    }
+    // link: [ 
     //     // <link rel="stylesheet" href="https://myawesome-lib.css">
     //     { rel: 'stylesheet', href: 'https://awesome-lib.css' }
     // ]
